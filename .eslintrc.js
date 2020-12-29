927e798602c9c348b7cf19e4ef6ec4c12b769761
Sebastian Kasanzew <sebkasanzew@gmail.com>
2020-12-29 03:07:19 +0100

    Update some packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   root: true,
   extends: ['@twihike'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
   parserOptions: {
     project: ['./tsconfig.json', './tests/e2e/tsconfig.json'],
   },
