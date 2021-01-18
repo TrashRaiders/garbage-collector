3756db8f08974375651f198833901d882584f582
Sebastian Kasanzew <sebastian.kasanzew@regiocom.com>
2021-01-18 11:42:24 +0100

    Fix cypress tests hanging

--- tests/e2e/support/index.js
+++ tests/e2e/support/index.js
@@ -1,3 +1,8 @@
 // https://on.cypress.io/configuration
 
 import './commands'
+
+beforeEach(() => {
+  // Used in the frontend to disable features that only fail in Cypress tests
+  window.localStorage.setItem('cypress', true)
+})
