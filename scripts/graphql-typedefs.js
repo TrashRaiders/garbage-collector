df1fb8d261324add0481487960b65e386af278bd
Sebastian Kasanzew <sebastian.kasanzew@regiocom.com>
2021-01-28 08:29:15 +0100

    Restructure frontend to make graphql great again
    Add GraphQL autocomplete extension to VSCode
    
    Add example mutation 'createShop'
    
    Attempt to ignore deepCode analysis for generated files
    
    Name query to fix `Cannot find name 'QueryVariables'` error
    
    Move generated code to src/generated
    
    Remove template '/users' page and 'react-table'
    
    Add sign in/up dummy mutations
    
    So we don't need to remove the frontend pages, as we will need them later anyway
    
    Remove Sign In/Up requests
    
    Move schema to `src/`

--- scripts/graphql-typedefs.js
+++ scripts/graphql-typedefs.js
@@ -1,17 +1,14 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
 const schema = fs
-  .readFileSync(path.join(__dirname, '../../nestjs-starter/schema.graphql'))
+  .readFileSync(path.join(__dirname, '../src/graphql/schema/stargate.graphql'))
   .toString()
-  .replace(/`/g, '');
+  .replace(/`/g, '')
 
 const schemaTs = `const s = \`${schema}\`;
 
 export default s;
-`;
+`
 
-fs.writeFileSync(
-  path.join(__dirname, '../src/graphql/generated/typedefs.ts'),
-  schemaTs,
-);
+fs.writeFileSync(path.join(__dirname, '../src/generated/typedefs.ts'), schemaTs)
---
+++ src/graphql/schema/README.md
@@ -0,0 +1,7 @@
+## stargate.graphql
+
+The primary schema for all queries and mutations used in the frontend.
+
+## database.graphql
+
+// TODO @rongo, please add a description, why we need this here.
