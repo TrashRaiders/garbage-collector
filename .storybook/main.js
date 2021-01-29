64c3672f18f8a7ad726dc71601c4c247b560765d
Sebastian Kasanzew <sebastian.kasanzew@regiocom.com>
2021-01-29 21:46:25 +0100

    Remove storycap addon for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
-  addons: ['storycap/register'],
+  addons: [],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,7 @@
 import { addDecorator, addParameters } from '@storybook/react'
-import { withScreenshot } from 'storycap'
 import { RouterContext } from 'next/dist/next-server/lib/router-context'
 import Router from 'next/router'
 
 addDecorator((storyFn) => (
   <RouterContext.Provider value={Router}>{storyFn()}</RouterContext.Provider>
 ))
-addDecorator(withScreenshot)
-addParameters({
-  screenshot: {},
-})
