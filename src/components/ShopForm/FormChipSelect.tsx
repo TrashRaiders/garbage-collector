1becacb7ec3da34a106e11d39c7bb476486b28b6
Sebastian Kasanzew <sebastian.kasanzew@regiocom.com>
2021-02-07 22:38:35 +0100

    Replace value validation with custom return value
    using the 'setValueAs' method in 'rules'

--- src/components/ShopForm/FormChipSelect.tsx
+++ src/components/ShopForm/FormChipSelect.tsx
@@ -1,6 +1,6 @@
 import { TextField } from '@material-ui/core'
 import { Autocomplete } from '@material-ui/lab'
-import React, { useState } from 'react'
+import React from 'react'
 import { Control, ValidationValueMessage, useController } from 'react-hook-form'
 
 export interface IChipItem {
@@ -18,27 +18,23 @@ interface IFormChipSelectProps {
   control: Control
 }
 
+type ReturnValue = string[]
+
+function setValueAs(value: IChipItem[]): ReturnValue {
+  return value.map((item) => item.label) || []
+}
+
 export default function FormChipSelect(
   props: IFormChipSelectProps,
 ): React.ReactElement {
   const { label, name, options, control, required, error } = props
-
-  const [hasError, setHasError] = useState(error)
-
-  const validateInputLength = (values) => {
-    setHasError(false)
-    if (values.length === 0) {
-      setHasError(true)
-    }
-    return values.length === 0 ? required : undefined
-  }
 
   const {
     field: { onChange, ...selectChipProps },
   } = useController({
     name,
     control,
-    rules: { validate: validateInputLength },
+    rules: { required, setValueAs },
     defaultValue: [],
   })
 
@@ -55,7 +51,7 @@ export default function FormChipSelect(
       renderInput={(params) => (
         <TextField
           {...params}
-          error={hasError}
+          error={error}
           variant="outlined"
           label={label}
           InputLabelProps={{
