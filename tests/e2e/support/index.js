66f6ffb06fb8e5df5d30bb07d7456b7c9fd505d8
Sebastian Kasanzew <sebkasanzew@gmail.com>
2021-02-12 20:02:57 +0100

    Fix storybook and e2e failing

--- tests/e2e/support/index.js
+++ tests/e2e/support/index.js
@@ -2,8 +2,6 @@
 
 import { configure } from '@testing-library/cypress'
 
-import { THEME_COOKIE } from '../../../src/contexts/dark-mode'
-
 import './commands'
 
 configure({ testIdAttribute: 'data-test-id' })
@@ -12,5 +10,5 @@ beforeEach(() => {
   // Used in the frontend to disable features that only fail in Cypress tests
   window.localStorage.setItem('cypress', true)
 
-  cy.setCookie(THEME_COOKIE, 'dark')
+  cy.setCookie('theme', 'dark')
 })
