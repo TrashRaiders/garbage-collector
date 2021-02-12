14a535190981971241d3545ecc3b32d5036d7469
Sebastian Kasanzew <sebastian.kasanzew@regiocom.com>
2021-02-12 18:13:51 +0100

    Create theme files for better readability

--- src/components/CommonProviders/MyThemeProvider.tsx
+++ src/components/CommonProviders/MyThemeProvider.tsx
@@ -1,9 +1,9 @@
-import { blueGrey, grey, lightGreen } from '@material-ui/core/colors'
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
-import { ThemeOptions } from '@material-ui/core/styles/createMuiTheme'
 import React from 'react'
 
 import { DarkModeContext } from '../../contexts/dark-mode'
+import { theme as darkTheme } from 'themes/dark'
+import { theme as lightTheme } from 'themes/light'
 
 // Light Theme example: https://material.io/resources/color/#!/?view.left=0&view.right=0&secondary.color=455A64&primary.color=7CB342
 
@@ -15,29 +15,7 @@ function MyThemeProvider(props: {
   const [darkMode] = React.useContext(DarkModeContext)
   const theme = React.useMemo(() => {
     const { isDarkMode } = darkMode
-    const themeProps: ThemeOptions = {
-      palette: {
-        type: isDarkMode ? 'dark' : 'light',
-        primary: {
-          main: isDarkMode ? lightGreen[200] : lightGreen[900],
-          contrastText: isDarkMode ? '#030303' : '#FEFEFE',
-        },
-        secondary: {
-          main: isDarkMode ? blueGrey[400] : blueGrey[700],
-        },
-        text: {
-          primary: isDarkMode ? grey[100] : grey[900],
-          secondary: isDarkMode ? grey[200] : grey[800],
-        },
-      },
-    }
-    if (isDarkMode && themeProps.palette) {
-      themeProps.palette.background = {
-        default: '#202020',
-        paper: '#303030',
-      }
-    }
-    return createMuiTheme(themeProps)
+    return createMuiTheme(isDarkMode ? darkTheme : lightTheme)
   }, [darkMode])
 
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>
