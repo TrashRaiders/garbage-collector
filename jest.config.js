27a5ac0aa51bb966b30b469430256c41367e13e2
Sebastian Kasanzew <sebkasanzew@gmail.com>
2021-02-14 13:25:56 +0100

    Fix combined coverage report

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,14 @@
 module.exports = {
+  roots: ['<rootDir>'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
   testPathIgnorePatterns: [
     '<rootDir>/.next/',
     '<rootDir>/node_modules/',
     '<rootDir>/tests/e2e/',
   ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  },
   collectCoverageFrom: [
     'src/**/*.js',
     '!**/*.test.js',
@@ -11,10 +16,15 @@ module.exports = {
     '!**/*.stories.js',
     '!**/stories.js',
   ],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+  },
   // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
   // The directory where Jest should output its coverage files
   coverageDirectory: 'jest-coverage',
-  // The test environment that will be used for testing
-  testEnvironment: 'node',
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
 }
