324e4c48e7d9412ff1ef0504c0a0ed7690f295ac
Sebastian Kasanzew <sebkasanzew@gmail.com>
2021-02-23 09:35:22 +0100

    Show english localized texts in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,16 @@
 import { addDecorator, addParameters } from '@storybook/react'
 import { RouterContext } from 'next/dist/next-server/lib/router-context'
 import Router from 'next/router'
+import I18nProvider from 'next-translate/I18nProvider'
+
+import common from '../locales/en/common.json'
 
 addDecorator((storyFn) => (
   <RouterContext.Provider value={Router}>{storyFn()}</RouterContext.Provider>
+))
+
+addDecorator((storyFn) => (
+  <I18nProvider lang="en" namespaces={{ common }}>
+    {storyFn()}
+  </I18nProvider>
 ))
