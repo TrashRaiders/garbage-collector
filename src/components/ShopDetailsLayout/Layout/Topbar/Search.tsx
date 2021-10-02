024347a732356c3c998dd30a6ba7b0dd81c0a071
Sebastian Kasanzew <sebkasanzew@gmail.com>
2021-10-02 15:50:17 +0200

    Remove deprecated mui functions

--- src/components/ShopDetailsLayout/Layout/Topbar/Search.tsx
+++ src/components/ShopDetailsLayout/Layout/Topbar/Search.tsx
@@ -1,5 +1,5 @@
 import { InputBase, Theme } from '@material-ui/core'
-import { fade, makeStyles } from '@material-ui/core/styles'
+import { alpha, makeStyles } from '@material-ui/core/styles'
 import SearchIcon from '@material-ui/icons/Search'
 import useTranslation from 'next-translate/useTranslation'
 import React from 'react'
@@ -10,9 +10,9 @@ const useStyles = makeStyles((theme: Theme) => ({
   search: {
     position: 'relative',
     borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
     '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
     },
     marginRight: theme.spacing(2),
     marginLeft: 0,
