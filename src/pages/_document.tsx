54a07c5d4169e83ca4293e2fcd62a5d33b971603
Sebastian Kasanzew <sebkasanzew@gmail.com>
2021-10-02 15:48:51 +0200

    Upgrade to next v11

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,15 @@
 import { ServerStyleSheets } from '@material-ui/core/styles'
-import {
-  AppContextType,
-  AppInitialProps,
-  AppPropsType,
+import { NextComponentType } from 'next'
+import { AppInitialProps } from 'next/app'
+import { AppContextType, AppPropsType } from 'next/dist/shared/lib/utils'
+import Document, {
   DocumentContext,
   DocumentInitialProps,
-  NextComponentType,
-  RenderPageResult,
-} from 'next/dist/next-server/lib/utils'
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document'
 import { NextRouter } from 'next/router'
 import React from 'react'
 
@@ -34,33 +35,11 @@ export default class MyDocument extends Document {
 MyDocument.getInitialProps = async (
   context: DocumentContext,
 ): Promise<DocumentInitialProps> => {
-  // Resolution order
-  //
-  // On the server:
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. document.getInitialProps
-  // 4. app.render
-  // 5. page.render
-  // 6. document.render
-  //
-  // On the server with error:
-  // 1. document.getInitialProps
-  // 2. app.render
-  // 3. page.render
-  // 4. document.render
-  //
-  // On the client
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. app.render
-  // 4. page.render
-
   // Render app and page and get the context of the page with collected side effects.
   const sheets = new ServerStyleSheets()
   const originalRenderPage = context.renderPage
 
-  context.renderPage = (): RenderPageResult | Promise<RenderPageResult> =>
+  context.renderPage = () =>
     originalRenderPage({
       enhanceApp:
         (
