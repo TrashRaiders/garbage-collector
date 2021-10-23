95b20ef6b91db935601f902aa9aa735342d23835
Raphael Zöllner <raphael.zoellner@outlook.com>
2021-10-23 15:43:42 +0200

    Add workaround to not pass files on .eslintignore to eslint with lint-staged which would produce a warning

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,14 @@
 const fs = require('fs')
 const micromatch = require('micromatch')
 
+// Workaround to not match files in .eslintignore since a warning will be produced when a single file
+// (which is contained in .eslintignore)
+// ref: https://github.com/okonet/lint-staged/issues/711
 const ignore = fs.readFileSync('.eslintignore', 'utf8')
 const lines = ignore.match(/[^\n\r]+/g)
 const jsRunners = (files) => {
   const match = micromatch.not(files, lines)
-  return match.map((file) => `eslint ${file} --fix --max-warnings=0`)
+  return match.map((file) => `eslint ${file} --fix --max-warnings=1`)
 }
 
 module.exports = {
